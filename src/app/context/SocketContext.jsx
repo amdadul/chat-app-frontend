30b21222f23d8d50b7834e67ce764e90a7dfc10b
amdadul haque <amdadulhaque05@gmail.com>
2025-05-06 23:52:05 +0600

    many issue fix

--- src/app/context/SocketContext.jsx
+++ src/app/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketProvider = ({ children, session }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:3010"); // Replace with your WebSocket server URL
+    const newSocket = io("https://chatapi.motionsoft.com.bd/"); // Replace with your WebSocket server URL
     setSocket(newSocket);
 
     return () => newSocket.disconnect();
